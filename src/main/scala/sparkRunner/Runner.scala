012dd88db4e98d12b8837e4d9a3895a99bd4b050
Danny Lee <danny.lee@revature.net>
2020-11-19 16:01:22 -0500

    Add null check to when reading json files in to 5-tuple. Changed writeFile method signature to fit new datatypes of 5-tuple. Commented out the header line in csv

--- src/main/scala/sparkRunner/Runner.scala
+++ src/main/scala/sparkRunner/Runner.scala
@@ -15,6 +15,8 @@ object Runner {
       .master("local[*]")
       .getOrCreate()
 
+//    spark.sparkContext.setLogLevel("TRACE")
+
     val trendSchema = new StructType()
       .add("as_of", StringType)
       .add("created_at", StringType)
@@ -30,7 +32,7 @@ object Runner {
           .add("url", StringType)
         ))
 
-    val df = spark.read.schema(trendSchema).json("input-old")
+    val df = spark.read.schema(trendSchema).json("input")
 
     val file = new File("output.csv")
 
@@ -40,12 +42,12 @@ object Runner {
 
     val buffered_writer = new BufferedWriter(file_writer)
 
-    buffered_writer
-      .write("\"Trend Text\",\"Location\",\"Timestamp\",\"Rank\",\"Tweet Volume\"\n")
+//    buffered_writer
+//      .write("\"Trend Text\",\"Location\",\"Timestamp\",\"Rank\",\"Tweet Volume\"\n")
 
     buffered_writer.close()
 
-    def writeFile(lines: List[(Any, String, String, Int, Any)]) {
+    def writeFile(lines: List[(Any, Any, Any, Int, Int)]) {
       try {
         val file = new File("output.csv")
 
@@ -59,10 +61,10 @@ object Runner {
           buffered_writer
             .write("\"%s\",\"%s\",\"%s\",%d,%d\n"
               .format(
-                line._1.toString,
-                line._2,
-                line._3,
-                line._4,
+                { if (null == line._1) 0 else line._1.toString },
+                { if (null == line._2) 0 else line._2 },
+                { if (null == line._3) 0 else line._3 },
+                { if (null == line._4) 0 else line._4 },
                 { if (null == line._5) 0 else line._5.toString.toInt }
               ))
         }
@@ -87,14 +89,22 @@ object Runner {
         .getAs[mutable.WrappedArray[Row]](3)
         .zipWithIndex.map { case (struct, index) =>
         // trend text, location, time stamp, rank, tweet volume
-        (struct(0), location, as_of, index + 1, struct(3))
+
+        (
+          { if (null == struct(0)) 0 else struct(0) },
+          { if (null == location) 0 else location },
+          { if (null == as_of) 0 else as_of },
+          { if (null == index + 1) 0 else index + 1 },
+          { if (null == struct(3)) 0 else struct(3).toString.toInt }
+        )
       }.toList
 
       writeFile(trends)
 
     })
 
-    spark.stop()
+
+
   }
 }
 
